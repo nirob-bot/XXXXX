966e996648d47f34f80d54f94b042981bc4733d5
ARIJIT <arijitnaha2@gmail.com>
2025-08-10 19:02:52 +0530

    Create 0Alya.js

---
+++ scripts/cmds/0Alya.js
@@ -0,0 +1,31 @@
+module.exports = {
+config: {
+name: "alya",
+version: "1.0",
+author: "Tokodori_Frtiz",//remodified by cliff
+countDown: 5,
+role: 0,
+shortDescription: "no prefix",
+longDescription: "no prefix",
+category: "auto 🪐",
+},
+
+onStart: async function(){}, 
+onChat: async function({ event, message, getLang }) {
+if (event.body && event.body.toLowerCase() === "alya") {
+return message.reply({
+body: `
+  𝐎𝐰𝐧𝐞𝐫 👑: \⃝—͟͟͞͞ 𝐀 𝐑 𝐈 𝐉 𝐈 𝐓𓆪_//
+
+ 𝐣𝐮𝐬𝐭 𝐬𝐚𝐲 𝐛𝐨𝐭/𝐛𝐛𝐲 𝐟𝐨𝐫 𝐭𝐚𝐥𝐤 𝐭𝐨 𝙰𝙻𝚈𝙰-𝙱𝙾𝚃🎀
+
+ 𝐞𝐧𝐣𝐨𝐲 𝐚𝐧𝐝 𝐡𝐚𝐯𝐞 𝐚 𝐟𝐮𝐧 𝐰𝐢𝐭𝐡 𝐦𝐲 𝐛𝐨𝐭
+ | (• ◡•)|🐱 
+
+
+\n\n\n  `,
+attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/b0n4vy.mp4")
+});
+}
+}
+}
